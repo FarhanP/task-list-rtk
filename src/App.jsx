a50b941379f9835cad5279ec17081ee46945e390
farhancodes <pmdfarhan@gmail.com>
2025-02-10 22:41:59 +0530

    Removed strong tag

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="w-full flex-grow max-w-2xl mx-auto flex flex-col justify-center items-center">
       <h1 className="font-bold text-center text-4xl my-10">
-        U.R <strong className="text-green-600">Task</strong> List
+        U.R <span className="text-green-600">Task</span> List
       </h1>
       <div className="flex justify-center gap-12 items-center flex-col">
         <TodoForm />
