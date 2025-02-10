f439a1f542d58c794d2003afd99d0f8ff5ebeb1c
farhancodes <pmdfarhan@gmail.com>
2025-02-10 22:57:38 +0530

    Footer in different folder

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,5 @@ import { store } from "./app/store.js";
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
-    <div className="flex">
-      <footer className="py-5 absolute left-0 right-0 bottom-0 z-50 text-center">
-        Made with <span className="text-green-600">&hearts;</span> by{" "}
-        <b className="font-bold">Farhan</b>
-      </footer>
-    </div>
   </Provider>
 );
