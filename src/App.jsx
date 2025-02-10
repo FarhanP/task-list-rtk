d37808c175884746352395086fc091f8dfb8272b
farhancodes <pmdfarhan@gmail.com>
2025-02-10 22:53:23 +0530

    Fixed footer issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import TodoList from "./components/TodoList";
 
 function App() {
   return (
-    <div className="w-full flex-grow max-w-2xl mx-auto flex flex-col justify-center items-center">
+    <div className="w-full max-w-2xl mx-auto flex flex-col justify-center items-center">
       <h1 className="font-bold text-center text-4xl my-10">
         U.R <span className="text-green-600">Task</span> List
       </h1>
